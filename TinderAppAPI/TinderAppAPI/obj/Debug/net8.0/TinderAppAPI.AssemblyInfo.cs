0aac285d1d54d85f8e0dc3af2efeb0dbaa7b662a
Astragor <101702563+Astrikkk@users.noreply.github.com>
2024-11-30 12:57:22 +0100

    no-change

--- TinderAppAPI/TinderAppAPI/obj/Debug/net8.0/TinderAppAPI.AssemblyInfo.cs
+++ TinderAppAPI/TinderAppAPI/obj/Debug/net8.0/TinderAppAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TinderAppAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99395cac6a2e821091571ceb16b9a9c5b3c5b5e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb46e6807b5a2698c1163c4c4c25bbea0e8013e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TinderAppAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TinderAppAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
